888529347621465b1cbf8f59440e1863477b80e6
Triet Nguyen <>
2025-06-08 11:15:55 +0700

    submit TwoIntegerSum

--- src/DuplicateInteger/Program.cs
+++ src/DuplicateInteger/Program.cs
@@ -1,37 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-bool HasDuplicate(int[] nums) {
+var input = new[] { 1, 2, 3, 3 };
+Console.WriteLine(HasDuplicate(input) ? "The array has duplicates." : "The array does not have duplicates.");
+
+return;
+
+bool HasDuplicate(int[] nums)
+{
     var length = nums.Length;
     if (length <= 1) return false;
 
     var head = 0;
-    while (head <= length - 2) {
+    while (head <= length - 2)
+    {
         var cur = head + 1;
 
-        while (cur <= length - 1) {
-            if (nums[head] == nums[cur]) {
+        while (cur <= length - 1)
+        {
+            if (nums[head] == nums[cur])
+            {
                 return true;
             }
 
             cur++;
         }
-            
+
         head++;
     }
 
     return false;
 }
-
-int[] nums = [1, 2, 3, 3];
-Console.WriteLine(HasDuplicate(nums) ? "The array has duplicates." : "The array does not have duplicates.");
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/TwoIntegerSum/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var nums1 = new int[] { 3, 4, 5, 6 };
+var nums2 = new int[] { 4, 5, 6 };
+var target = 10;
+var result = TwoSum(nums2, 10);
+Console.WriteLine("TwoSum output = [{0}]", string.Join(", ", result));
+
+return;
+
+int[] TwoSum(int[] nums, int target)
+{
+    if (nums.Length < 2)
+    {
+        return Array.Empty<int>();
+    }
+
+    if (nums.Length == 2)
+    {
+        return new[] { 0, 1 };
+    }
+
+    int i;
+    var j = 0;
+    for (i = 0; i < nums.Length - 1; i++)
+    {
+        var addend1 = nums[i];
+        for (j = i + 1; j < nums.Length; j++)
+        {
+            var append2 = nums[j];
+            if (append2 + addend1 == target)
+            {
+                return new[] { i, j };
+            }
+        }
+    }
+    return new[] { i, j };
+}
