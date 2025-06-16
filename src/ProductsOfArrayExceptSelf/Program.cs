5130c0866589298db0c53457a7189bd0fd796f4b
Triet Nguyen <>
2025-06-16 17:44:30 +0700

    submit Products of Array Except Self

---
+++ src/ProductsOfArrayExceptSelf/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = new[] { 1, 2, 4, 6 };
+input = new[] { -1, 0, 1, 2, 3 };
+
+var output = ProductExceptSelf(input);
+
+Console.WriteLine($"Input: [{string.Join(", ", input)}]");
+Console.WriteLine($"Output = [{string.Join(", ", output)}]");
+
+return;
+
+int[] ProductExceptSelf(int[] nums)
+{
+    var result = new List<int>(nums.Length);
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var j = 0;
+        result.Add(1);
+        while (j < nums.Length)
+        {
+            if (i == j)
+            {
+                j++;
+                continue;
+            }
+
+            result[i] *= nums[j++];
+        }
+    }
+
+    return result.ToArray();
+}
